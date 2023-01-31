b18fcc9ca41d094b8c8bb80def14969ead7be7d1
ds-ajeetpal <ajeet.pal@dotsquares.com>
2023-01-31 20:13:24 +0530

    footer header

--- .artifact-output/plugins/Generator/assets/server/robots.e11014cc.js
+++
@@ -1,2 +0,0 @@
-import{s as t}from"../static/global-c9b2facd.js";const o={name:"robots"},e=()=>"robots.txt",s=r=>`Sitemap: ${t}/sitemap.xml
-  User-agent: /`;export{o as config,e as getPath,s as render};
